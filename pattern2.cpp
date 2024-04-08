85c9fb7508fa29e8117ce069851d235fe8959a41
Owais <zaidalit00@gmail.com>
2024-04-08 16:12:55 +0530

    pattern 2

--- pattern2.cpp
+++ pattern2.cpp
@@ -4,10 +4,34 @@ using namespace std;
 int main()
 {
 
-cout<<"Hello";
+    int num;
 
+    cout << "Enter a number" << endl;
+    cin >> num;
 
-
+    // Outer Loop
+    for (int i = 1; i <= num; i++)
+    {
+        // Inner Loop
+        for (int j = 1; j <= num; j++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
+
+/*
+
+if n = 5;
+---------
+
+1 1 1 1 1
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+5 5 5 5 5
+
+*/
